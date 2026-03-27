42963596205a1d5f3c36b28b9c09f698f5ff2189
yjtc-arch <19959956002@163.com>
2026-03-27 21:11:40 +0800

    add eventSystem
    老项目的事件系统

---
+++ Assets/Tools/EventSystem/EventManage.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine.EventSystems;
+using EventSystem;
+
+namespace EventSystem
+{
+	public class EventManageData
+	{
+		public EventManageEnum EventManageEnum;
+		public List<EventGameFun> list = new List<EventGameFun>();
+	}
+
+	public delegate void EventGameFun(Object obj);
+}
+
+
+
+public class EventManage
+{
+	static List<EventManageData> eventList = new List<EventManageData>();
+	public static void AddEvent(EventManageEnum enumType, EventGameFun eventFun)
+	{
+		for (int i = 0; i < eventList.Count; i++)
+		{
+			if (eventList[i].EventManageEnum == enumType)
+			{
+				eventList[i].list.Add(eventFun);
+				return;
+			}
+		}
+
+		EventManageData data = new EventManageData();
+		data.EventManageEnum = enumType;
+		data.list.Add(eventFun);
+
+		eventList.Add(data);
+	}
+
+	public static void RemoveEvent(EventManageEnum enumType, EventGameFun eventFun)
+	{
+		for (int i = 0; i < eventList.Count; i++)
+		{
+			if (eventList[i].EventManageEnum == enumType)
+			{
+				for (int j = 0; j < eventList[i].list.Count; j++)
+				{
+					if (eventList[i].list[j] == eventFun)
+					{
+						eventList[i].list.RemoveAt(j);
+						return;
+					}
+				}
+			}
+		}
+	}
+
+	public static void SendEvent(EventManageEnum enumType, Object obj)
+	{
+		for (int i = 0; i < eventList.Count; i++)
+		{
+			if (eventList[i].EventManageEnum == enumType)
+			{
+				for (int j = eventList[i].list.Count - 1; j > -1; j--)
+				{
+					if (eventList[i].list[j] != null && !eventList[i].list[j].Target.IsUnityNull())
+						eventList[i].list[j](obj);
+					else
+						eventList[i].list.RemoveAt(j);
+				}
+				return;
+			}
+		}
+	}
+
+	public static void CheckNullFun()
+	{
+		for (int i = 0; i < eventList.Count; i++)
+		{
+			for (int j = eventList[i].list.Count - 1; j > -1; j--)
+			{
+				if (eventList[i].list[j] == null || eventList[i].list[j].Target == null)
+					eventList[i].list.RemoveAt(j);
+			}
+		}
+	}
+}
---
+++ Assets/Tools/EventSystem/EventManageEnum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public enum EventManageEnum
+{
+
+	
+    #region 玉皆透澈声明的事件
+    
+
+    
+    #endregion
+
+
+}
