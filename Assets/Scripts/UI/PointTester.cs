9b80b401a17772b6eb42967bf86e5da10cc9d56e
Alpleateauh <2024302111096@whu.edu.cn>
2026-04-16 23:46:55 +0800

    unity主人我是您的星怒立口牙

--- Assets/Scripts/UI/PointTester.cs
+++ Assets/Scripts/UI/PointTester.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
 [RequireComponent(typeof(RectTransform))]
-public class PointTester : MonoBehaviour
+public class PointTester : MonoBehaviour, IPointerClickHandler
 {
     [Header("触发的事件 (可在此绑定Confirm/Skip)")]
     public UnityEvent onClickAction;
@@ -11,45 +12,30 @@ public class PointTester : MonoBehaviour
     public bool hideOnClick = false;
 
     private RectTransform rectTransform;
-    private Canvas parentCanvas;
-    
-    // 用于保存四个角的数组
-    private Vector3[] worldCorners = new Vector3[4];
+    private Camera cachedCamera;
 
     private void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
-        parentCanvas = GetComponentInParent<Canvas>();
+        Canvas parentCanvas = GetComponentInParent<Canvas>();
+        if (parentCanvas != null && parentCanvas.renderMode != RenderMode.ScreenSpaceOverlay)
+        {
+            cachedCamera = parentCanvas.worldCamera;
+        }
     }
 
-    private void Update()
+    public void OnPointerClick(PointerEventData eventData)
     {
-        // 监听鼠标左键点击
-        if (Input.GetMouseButtonDown(0))
+        if (eventData.button != PointerEventData.InputButton.Left) return;
+
+        if (RectTransformUtility.RectangleContainsScreenPoint(rectTransform, eventData.position, cachedCamera))
         {
-            // 实时获取当前物体在世界空间中的四个角坐标：
-            // [0] 左下, [1] 左上, [2] 右上, [3] 右下
-            rectTransform.GetWorldCorners(worldCorners);
+            Debug.Log($"[PointTester] 成功点击到【{gameObject.name}】的四个角框定区域内！");
+            onClickAction?.Invoke();
 
-            // 获取相应的相机 (如果是 Overlay 模式则为 null)
-            Camera currentCamera = null;
-            if (parentCanvas != null && parentCanvas.renderMode != RenderMode.ScreenSpaceOverlay)
+            if (hideOnClick)
             {
-                currentCamera = parentCanvas.worldCamera;
-            }
-
-            // RectTransformUtility.RectangleContainsScreenPoint 的底层逻辑就是：
-            // 检测传递进来的屏幕点(Input.mousePosition)是否在这四个角(worldCorners)构成的矩形区域内。
-            // 这个方法完美兼容了被旋转、缩放或者不同Canvas下的情况
-            if (RectTransformUtility.RectangleContainsScreenPoint(rectTransform, Input.mousePosition, currentCamera))
-            {
-                Debug.Log($"[PointTester] 成功点击到【{gameObject.name}】的四个角框定区域内！");
-                onClickAction?.Invoke(); // 触发UnityEvent
-
-                if (hideOnClick)
-                {
-                    gameObject.SetActive(false); // 隐藏自己
-                }
+                gameObject.SetActive(false);
             }
         }
     }
@@ -62,3 +48,4 @@ public class PointTester : MonoBehaviour
         gameObject.SetActive(false);
     }
 }
+
